726055bc469f334473406a6de6b5104b51bc2702
이정원 <jwon133@gmail.com>
2025-03-27 02:38:10 +0900

    Railway 배포 준비

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
- 
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, "public")));
+
+// 모든 요청을 index.html로 라우팅 (SPA 지원)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`서버가 포트 ${PORT}에서 실행 중입니다`);
+});
